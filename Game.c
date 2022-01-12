3aaa7583c01996a663db1c76b5b7bda0e0d205ce
Tanmay <96858809+TanmayPr7@users.noreply.github.com>
2022-01-12 23:15:32 +0530

    Writting function for Game

--- Game.c
+++ Game.c
@@ -11,3 +11,28 @@ void setup()
     int num;
     srand(time(0));
     num=rand()%100;
+    
+    int no_guess=0;
+    printf("                                           ****Guess the number between 1 to 100:)****\n");
+
+    while(guess!=num)
+    {
+        scanf("%d",&guess);
+        if(guess>num)
+        {
+            printf("Lower number plz:)\n");
+        }
+        else if(guess<num)
+        {
+            printf("Higher number plz:)\n");
+        }
+        else
+        {
+            printf("\n");
+            printf("                                        (:Congrats you gussed the number in %d attempts:)\n",no_guess);
+            printf("                                          Thanks for Playing Our Game please Rate Us:)\n");
+            printf("************************************************************************************************************************");
+        }
+        no_guess++;
+    }
+}
